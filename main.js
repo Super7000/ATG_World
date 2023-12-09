9fa70b7b354daa0cf6853fc48cca2847ec947b4e
Subrata Chowdhury <subratachowdhury7000@gmail.com>
2023-12-09 21:19:45 +0530

    final

--- main.js
+++ main.js
@@ -1,52 +1,71 @@
-document.querySelector(".close-sign-up-icon").addEventListener("click",()=>{
-    toggleSignUpPopup();
-})
-document.querySelector(".create-account-container").addEventListener("click",()=>{
-    toggleSignUpPopup();
-})
-function toggleSignUpPopup(){
+function toggleSignUpPopup() {
     document.querySelector(".sign-up-popup").classList.toggle("active");
     document.querySelector(".sign-up-popup-shadow").classList.toggle("active");
     document.querySelector(".close-sign-up-icon").classList.toggle("active");
 }
 
+function toggleSignUp() {
+    let signUpPopUp = document.querySelector(".sign-up-popup");
+    if (signUpPopUp.classList.toString().indexOf("sign-up-mode") !== -1) {
+        signUpPopUp.classList.remove("sign-up-mode");
+        signUpPopUp.classList.add("sign-in-mode");
 
-document.querySelector(".sign-up-text .sign-in-link-text").addEventListener("click",()=>{
-    let signUpCon = document.querySelector(".sign-up-container");
-
-    if(signUpCon.classList[1] == "sign-up-mode"){
-        signUpCon.classList.remove("sign-up-mode");
-        signUpCon.classList.add("sign-in-mode");
-        
-        document.querySelector(".sign-up-heading").innerHTML = "Sign In";
-        document.querySelector(".first-name").style.display = "none";
-        document.querySelector(".last-name").style.display = "none";
-        document.querySelector(".confirm-password").style.display = "none";
-        document.querySelector(".first-name").style.display = "none";
+        if (screen.width <= 768) {
+            document.querySelector(".sign-up-heading").innerHTML = "Sign In";
+        } else {
+            document.querySelector(".sign-up-heading").innerHTML = "Welcome Back!";
+        }
         document.querySelector(".sign-up-text .text").innerHTML = `Don’t have an account yet?`;
         document.querySelector(".sign-up-text .sign-in-link-text").innerHTML = `Create new for free!`;
-        document.querySelector(".privacy-policy-text").style.display = "none";
-        document.querySelector(".forgot-password-link").style.display = "grid";
+        document.querySelector(".sign-up .create-account-btn").innerHTML = `Sign In`;
+        document.querySelector(".submit-btn-container u").innerHTML = "or, Create Account";
+
+
     } else {
-        signUpCon.classList.add("sign-up-mode");
-        signUpCon.classList.remove("sign-in-mode");
+        signUpPopUp.classList.add("sign-up-mode");
+        signUpPopUp.classList.remove("sign-in-mode");
 
         document.querySelector(".sign-up-heading").innerHTML = "Create Account";
-        document.querySelector(".first-name").style.display = "block";
-        document.querySelector(".last-name").style.display = "block";
-        document.querySelector(".confirm-password").style.display = "block";
-        document.querySelector(".first-name").style.display = "block";
         document.querySelector(".sign-up-text .text").innerHTML = `Already have an account?`;
-        document.querySelector(".privacy-policy-text").style.display = "none";
-        document.querySelector(".forgot-password-link").style.display = "none";
         document.querySelector(".sign-up-text .sign-in-link-text").innerHTML = `Sign In`;
+        document.querySelector(".sign-up .create-account-btn").innerHTML = `Create Account`;
+        document.querySelector(".submit-btn-container u").innerHTML = "or, Sign In";
     }
+}
+
+function customClickListener(arrayOfClassOfBtns, onClickFuntion) {
+    arrayOfClassOfBtns.forEach((className) => {
+        document.querySelector(className).addEventListener("click", onClickFuntion);
+    })
+
+}
+
+let arrayOfClassOfBtnsForSignUpPopUp = [".close-sign-up-icon", ".create-account-container", ".top-join-group-btn", ".create-post-btn"];
+customClickListener(arrayOfClassOfBtnsForSignUpPopUp, toggleSignUpPopup)
+
+
+let arrayOfClassOfBtnsForSignUpToggle = [".sign-up-text .sign-in-link-text", ".submit-btn-container u"];
+customClickListener(arrayOfClassOfBtnsForSignUpToggle, toggleSignUp)
+
+document.querySelector(".location-input-box").addEventListener("focus", () => {
+    document.querySelector(".location-right-icon").src = "Icons/crossWithoutBorder.svg";
+})
+document.querySelector(".location-input-box").addEventListener("focusout", () => {
+    document.querySelector(".location-right-icon").src = "Icons/filledPen.svg";
 })
 
-
-document.querySelectorAll(".options-btn").forEach((optBtn)=>{
-    optBtn.addEventListener("click",(event)=>{
-        document.querySelector(".option-container").style.cssText = `top: ${event.clientY}px; left: ${event.clientX-160}px; display: grid;`;
+document.querySelectorAll(".options-btn").forEach((optBtn) => {
+    optBtn.addEventListener("click", (event) => {
+        optBtn.classList.add("active");
+        document.querySelector(".option-container").style.cssText = `top: ${event.clientY}px; left: ${event.clientX - 160}px; display: grid;`;
     })
 })
-window.onscroll = ()=>{document.querySelector(".option-container").style.cssText = `display: none;`;}
+
+window.onscroll = () => {
+    document.querySelector(".option-container").style.cssText = `display: none;`;
+    try{
+        document.querySelector(".options-btn.active").classList.remove("active");
+    } catch(err) {
+
+    }
+}
